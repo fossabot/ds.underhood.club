77e9343235b5b266ab5ba43e45091b04ce96d52e
tiulpin <viktor@tiulp.in>
2020-11-08 00:19:46 +0300

    :tada: Here we go again, initial commit

---
+++ authors.js
@@ -0,0 +1,22 @@
+import authorId from './helpers/author-id';
+
+export default authorId([
+  // post: false  
+  { username: 'andrey_cheptsov', start: '26 October 2020', first: '1320622896834842624', post: true, update: true},
+  { username: 'IAshrapov', start: '19 October 2020', first: '1318084080752361472', post: true, update: true},
+  { username: 'iggisv9t', start: '29 June 2020', first: '1277527866566115329', post: true, update: true},
+  { username: 'SergeyI49013776', start: '15 June 2020', first: '1272407663503081479', post: true, update: true},
+  { username: 'generall931', start: '1 June 2020', first: '1267385162050031616', post: true, update: true},
+  { username: 'gang1man', start: '25 May 2020', first: '1264839430398971908', post: true, update: true},
+  { username: 'artsiom_s', start: '18 May 2020', first: '1262383147519561731', post: true, update: true},
+  { username: 'Al_Grigor', start: '11 May 2020', first: '1259775988851970049', post: true, update: true},
+  { username: 'hexfaker', start: '4 May 2020', first: '1257238441215098881', post: true, update: true},
+  { username: 'XOR0SHA2wine', start: '27 Apr 2020', first: '1254675614545690628', post: true, update: true},
+  { username: 'rampeer', start: '20 Apr 2020', first: '1252171861951201281', post: true, update: true},
+  { username: 'scitator', start: '6 Apr 2020', first: '1247070245141000192', post: true, update: true},
+  { username: 'digitman_', start: '30 Mar 2020', first: '1244507961977966593', post: true, update: true },
+  { username: 'ermakovpetr', start: '23 Mar 2020', first: '1242011377276837889', post: true, update: true },
+  { username: 'ykashnitsky', start: '16 Mar 2020', first: '1239315724142817283', post: true, update: true },
+  { username: 'tez_romach', start: '9 Mar 2020', first: '1236771313496227840', post: true, update: true },
+  { username: 'AndLukyane', start: '29 Feb 2020', first: '1234173069209542658', post: true, update: true },
+]);
